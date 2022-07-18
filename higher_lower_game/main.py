afb4d88ce23ee834a5172545a03eb5cc844c258b
Dariusz Starosta <darstar.misc@gmail.com>
2022-07-18 20:49:18 +0200

    First commit high_low_game

---
+++ higher_lower_game/main.py
@@ -0,0 +1,12 @@
+from art import logo, vs
+from game_data import data
+
+print(len(data))
+print(data[1])
+dic_a = data[1]
+print(dic_a["name"])
+
+is_game_finished = False
+
+while is_game_finished == True:
+  
